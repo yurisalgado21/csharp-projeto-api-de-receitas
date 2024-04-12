e8a3b4d4221fd5663eff6a2f3c3fe2a46b923eb5
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2024-04-12 16:27:22 -0300

    req Desenvolva o endpoint POST /comment done

--- src/recipes-api/Controllers/CommentController.cs
+++ src/recipes-api/Controllers/CommentController.cs
@@ -24,7 +24,8 @@ public class CommentController : ControllerBase
     [HttpPost]
     public IActionResult Create([FromBody]Comment comment)
     {
-        throw new NotImplementedException();
+        _service.AddComment(comment);
+        return StatusCode(201, comment);
     }
 
     // 11 - Sua aplicação deve ter o endpoint GET /comment/:recipeName
